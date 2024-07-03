1d96001c78571afd7d18a00a16ab7bf136a22388
swizzlestix09 <angely.rodriguez@gmail.com>
2024-07-03 17:31:02 -0400

    adding data + adding that to gitignore

---
+++ solid-personal/src/app/lib/data.ts
@@ -0,0 +1,9 @@
+const aboutMe = [
+  {
+    id: `7775e3ff-e39c-4e7b-9401-60851c0c9b06`,
+    content: `Hey there! I'm a full-stack software engineer who thrives on both coding and creativity. Whether I'm enhancing high-traffic pages, mentoring budding developers, or advocating for crypto policies, I'm all about making a positive impact. 🖥
+    I started my journey with a web developer course on Udemy, which lead me to learning HTML/CSS/JS. After that course, I wanted to learn more, so I enrolled in a boot camp called Hack Reactor. It was an intense 12 weeks of learning full-stack web development. I actually had to take the immersive twice before passing, so I have extra training ;)
+    After that course, I was hired by hack reactor to be a immersive resident, helping new students learn and being their rock when they had questions or needed support. While I was doing that, I found a oppertunity with onramp.io to get hired as an apprentice by coinbase, and from an apprentice I turned into a front end engineer for a little over two years. I worked on their highly-trafficed asset pages - implementing redesigns and enhancing accessibility. I was then moved to the advocacy team - there was a big push in the company for crypto advocacy in politics, so we were tasked to develop some pages that would educate the use on politicians, their stance on crypto and crypto policies in general. Those pages became a 501c4 nonprofit platform called stand with crypto, which was its own website using next.js - react typescript and prisma. It has amassed over 1M+ advocates to date.
+    Coding is like a canvas for my creative mind, allowing me to constantly dismantle and rebuild with finesse. Plus, my minor obsession with styling ensures sleek, modern interfaces. When I'm not immersed in code, you'll find me on my PS5 🎮, indulging in knitting or sewing projects 🪡, or having quality time with my adorable cats 🐈‍⬛ and baby boy 👦.`,
+  },
+];
