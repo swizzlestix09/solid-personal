cb83664989fd63ed2349f047ceb22d84575a75e3
swizzlestix09 <angely.rodriguez@gmail.com>
2024-07-03 23:18:05 -0400

    seed successful

--- solid-personal/src/app/seed/route.ts
+++ solid-personal/src/app/seed/route.ts
@@ -4,16 +4,16 @@ import { aboutMe, projects, helpfulContent } from "../lib/data";
 const client = await db.connect();
 
 async function seedAboutMe() {
-  await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp`;
+  await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
 
   await client.sql`
-  CREATE TABLE IF NOT EXISTS aboutMe {
+  CREATE TABLE IF NOT EXISTS aboutMe (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-    content TEXT,
-  }
+    content TEXT
+  );
   `;
 
-  client.sql`
+  await client.sql`
   INSERT INTO aboutMe (id, content)
   VALUES(${aboutMe[0].id}, ${aboutMe[0].content})
   ON CONFLICT (id) DO NOTHING;
@@ -21,26 +21,26 @@ async function seedAboutMe() {
 }
 
 async function seedProjects() {
-  await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp`;
+  await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
   await client.sql`
-  CREATE TABLE IF NOT EXISTS projects {
+  CREATE TABLE IF NOT EXISTS projects (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
     title VARCHAR(255),
     content TEXT,
-    link TEXT NOT NULL,
-  }
+    link TEXT NOT NULL
+    )
   `;
 }
 
 async function seedHelpfulContent() {
-  await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp`;
+  await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
   await client.sql`
-  CREATE TABLE IF NOT EXISTS helpfulContent {
+  CREATE TABLE IF NOT EXISTS helpfulContent (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
     title VARCHAR(500),
     author VARCHAR(255),
-    link TEXT,
-  }
+    link TEXT
+  )
   `;
 }
 
@@ -48,6 +48,7 @@ export async function GET() {
   try {
     await client.sql`BEGIN`;
     await seedAboutMe();
+    console.log("seed me finished");
     await seedProjects();
     await seedHelpfulContent();
     await client.sql`COMMIT`;
